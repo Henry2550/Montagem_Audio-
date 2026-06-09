3dd62cb1206bdec32165b01aec01bd403fbe78c9
Henry2550 <viniihenry06@gmail.com>
2026-06-09 20:44:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,68 @@ let blocos = JSON.parse(localStorage.getItem("blocos")) || [];
 function adicionarBloco() {
     const nome = document.getElementById("nomeBloco").value;
 
-    if(nome === "") return;
+    if (!nome) return;
 
-    blocos.push(nome);
+    blocos.push({
+        nome: nome,
+        salas: []
+    });
 
-    localStorage.setItem("blocos", JSON.stringify(blocos));
-
+    salvarDados();
     atualizarLista();
 
     document.getElementById("nomeBloco").value = "";
 }
 
+function adicionarSala() {
+    const blocoIndex = document.getElementById("blocoSala").value;
+    const sala = document.getElementById("nomeSala").value;
+
+    if (sala === "") return;
+
+    blocos[blocoIndex].salas.push(sala);
+
+    salvarDados();
+    atualizarLista();
+
+    document.getElementById("nomeSala").value = "";
+}
+
+function salvarDados() {
+    localStorage.setItem("blocos", JSON.stringify(blocos));
+}
+
 function atualizarLista() {
-    const lista = document.getElementById("listaBlocos");
 
-    lista.innerHTML = "";
+    const listaBlocos = document.getElementById("listaBlocos");
+    const selectBloco = document.getElementById("blocoSala");
 
-    blocos.forEach(bloco => {
+    listaBlocos.innerHTML = "";
+    selectBloco.innerHTML = "";
+
+    blocos.forEach((bloco, index) => {
+
+        const option = document.createElement("option");
+        option.value = index;
+        option.textContent = bloco.nome;
+        selectBloco.appendChild(option);
+
         const li = document.createElement("li");
-        li.textContent = bloco;
-        lista.appendChild(li);
+
+        let html = `
+            <strong>${bloco.nome}</strong>
+            <ul>
+        `;
+
+        bloco.salas.forEach(sala => {
+            html += `<li>Sala ${sala}</li>`;
+        });
+
+        html += "</ul>";
+
+        li.innerHTML = html;
+
+        listaBlocos.appendChild(li);
     });
 }
 
